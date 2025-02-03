ea990a4ce042ae8bc54e908b6ff2b8efb6ed6821
Fobiak <saysanov2018@mail.ru>
2025-02-03 06:14:06 +0500

    шапка почти готова(добавил лого, кнопки и инпут)

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,13 +1,27 @@
-<script setup>
-
+<script setup lang="ts" >
+import { Search, Operation } from '@element-plus/icons-vue'
 </script>
 
 <template>
-  <header class="w-full h-24 md:h-20 bg-blue-800 text-white flex items-center px-4 md:px-8">
-    <h1 class="text-gray-800">Dmitriy</h1>
-
-
+  <header class="flex justify-between border-b border-gray-300 px-10 py-3">
+    <div class="flex items-center gap-4">
+      <img src="@/assets/recipe.svg" alt="recipes" class="w-12">
+      <div>
+        <h2 class="text-xl font-bold">Все рецепты</h2>
+        <p class="text-xs text-slate-400">Еда — это искусство, а ты художник</p>
+      </div>
+    </div>
+    <div class=" flex-1 flex items-center gap-2 px-10">
+      <el-input v-model="input" style="width: 300px" placeholder="Поиск по рецептам">
+        <template #suffix>
+          <el-button :icon="Operation" size="large" style="border: none; margin-right: -10px;" ></el-button>
+        </template>
+      </el-input>
+      <el-button plain :icon="Search" size="large" circle />
+    </div>
+    <div class="flex items-center">
+      <el-button plain>Главная</el-button>
+    </div>
   </header>
-
 </template>
 
