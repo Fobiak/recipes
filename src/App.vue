93d71037b2833a7bc8dbcd53d480b597929e1a79
Fobiak <saysanov2018@mail.ru>
2025-02-10 00:06:00 +0500

    создал футер

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,19 @@
   <div>
     <Header/>
     <router-view/>
+    <Footer v-if="showFooter"/>
   </div>
 </template>
 
 <script setup lang="ts">
 import Header from "@/components/header/Header.vue";
+import Footer from "@/components/footer/Footer.vue";
+import {onMounted, ref} from "vue";
+const showFooter = ref(false);
+
+onMounted(() => {
+  setTimeout(() => {
+    showFooter.value = true;
+  }, 8000); // 5 секунд
+});
 </script>
---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="bg-white rounded-lg shadow-sm m-4">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://all-recipes" class="hover:underline">Recipe™</a>. All Rights Reserved.
+    </span>
+      <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+          <a href="#" class="hover:underline me-4 md:me-6">About</a>
+        </li>
+        <li>
+          <a href="https://github.com/Fobiak" class="hover:underline me-4 md:me-6">GitHub</a>
+        </li>
+        <li>
+          <a href="https://t.me/dayrret" class="hover:underline me-4 md:me-6">Telegram</a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
